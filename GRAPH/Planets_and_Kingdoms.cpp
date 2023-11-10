0621216553d49e83e6f10947e14351e57e0426ab
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-11-10 20:33:14 +0530

    Create Planets_and_Kingdoms.cpp

---
+++ GRAPH/Planets_and_Kingdoms.cpp
@@ -0,0 +1,121 @@
+// हर हर महादेव
+// जय महाकाल जय महाकाल जय महाकाल जय महाकाल
+/* Author :- Himanshu Shekhar , IIIT Bhagalpur */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD = 1e9 + 7;
+const long long INF = 1e15;
+const int MAX_N = 200000+15;
+ 
+# define f(a,n,i) for(int i = a; i<n; i++)
+# define in(a) cin>>a
+# define in2(a,b) cin>>a>>b
+# define ll long long
+
+vector<int> graph[MAX_N];
+vector<int> graph2[MAX_N];
+
+int store[MAX_N];
+int scc = 0;
+
+
+void dfs(int node, vector<int> &vis, stack<int> &st){
+    vis[node] = 1;
+    for(auto it: graph[node]){
+        if(!vis[it]) dfs(it,vis,st);
+    }
+    st.push(node);
+}
+
+void dfs2(int node, vector<int> &vis){
+        vis[node] = 1;
+        for(auto it: graph2[node]){
+            if(!vis[it]){
+                dfs2(it,vis);
+            }
+        }
+        store[node]=scc;  // changes done here
+}
+
+void solve(){
+    ll n,m;
+    in2(n,m);
+
+
+    
+        for(int i{}; i<m; i++){
+
+        int u , v;
+
+        cin >> u >> v;
+
+        graph[u].push_back(v);
+
+        
+    }
+
+    vector<int> vis(n+1,0);
+
+    stack<int> st;
+
+
+    for(int i = 1; i<=n; i++){
+        if(!vis[i]) dfs(i,vis,st);
+    }
+
+
+    // reverse the edges
+    for(int i = 1; i<=n; i++){
+            vis[i]=0;
+    for(auto it: graph[i]){
+
+        graph2[it].push_back(i);
+    }
+}
+
+
+while(!st.empty()){
+
+    int vertex = st.top();
+    st.pop();
+
+        if(!vis[vertex]){
+            scc++;
+            dfs2(vertex,vis);
+        }
+        
+}
+
+
+cout << scc << endl;
+
+
+// changes done here
+for(int i = 1; i<=n; i++){
+    cout << store[i] << " ";
+}
+
+ fflush(stdout);
+       cout << flush;
+
+ }
+ 
+int main() {
+ 
+    #ifndef ONLINE_JUDGE
+//for getting input from input.txt
+freopen("input1.txt", "r", stdin);
+//for writing output to output.txt
+freopen("output.txt", "w", stdout);
+#endif
+ 
+ 
+// cout<<1<<endl;
+ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+       solve();
+ 
+       return 0;
+}
