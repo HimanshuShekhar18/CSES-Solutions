530f7565b6207d5d5621da14babcb6d4780f4818
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-12-07 22:02:33 +0530

    Update TreeDiameter.cpp

--- TREE/TreeDiameter.cpp
+++ TREE/TreeDiameter.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 using namespace std;
 
+
+//<---------------------------------------------------------------SINGLE DFS OR DYNAMIC PROGAMMING------------------------------------------------------------------------------>
+
 const int MAX_N = 200 * 1000 + 13;
 
 vector<int> adj[MAX_N]; // Adjacency list representation
@@ -62,5 +65,90 @@ freopen("output.txt", "w", stdout);
     }
     // Output the result (diameter of the tree)
     cout << res << endl;
+    return 0;
+}
+
+
+
+
+// <------------------------------------------------------------------------TWO DFS APPROACH---------------------------------------------------------------------------------------------->
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX_N = 200 * 1000 + 13;
+
+vector<vector<int>> v(MAX_N);
+vector<bool> visited(MAX_N);
+vector<int> depth(MAX_N);
+int farthestNode, maxDepth;
+
+
+// TWO DFS APPROACH: TC: O(n)
+void dfs(int node, int d) {
+    visited[node] = true;
+    depth[node] = d;
+
+    if (d > maxDepth) {
+        maxDepth = d;
+        farthestNode = node;
+    }
+
+    for (int child : v[node]) {
+        if (!visited[child]) {
+            dfs(child, d + 1);
+        }
+    }
+}
+
+int findDiameter(int startNode) {
+    fill(visited.begin(), visited.end(), false);
+    fill(depth.begin(), depth.end(), 0);
+
+    maxDepth = -1;
+    dfs(startNode, 0);
+
+    int nodeA = farthestNode;
+
+    fill(visited.begin(), visited.end(), false);
+    fill(depth.begin(), depth.end(), 0);
+
+    maxDepth = -1;
+    dfs(nodeA, 0);
+
+    return maxDepth;
+}
+
+int main() {
+
+
+    #ifndef ONLINE_JUDGE
+//for getting input from input.txt
+freopen("input1.txt", "r", stdin);
+//for writing output to output.txt
+freopen("output.txt", "w", stdout);
+#endif
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    // Step 1: Assume any node 'a' as the root
+    int rootNode = 1;
+
+    int diameter = findDiameter(rootNode);
+
+    cout << diameter << endl;
+
     return 0;
 }
